b19b770c94d1799126c4754f649224be648dcda7
nikhilraj021 <dnraj2001@gmail.com>
2024-11-21 14:08:42 +0530

    updated backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
+
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
+const Api = "https://jsonplaceholder.typicode.com/users";
+
+app.get("/users", async (req, res) => {
+    try {
+        const response = await axios.get(Api)
+        res.json(response.data)
+    } catch (e) {
+        console.log("error", e)
+        res.status(500)
+    }
+})
 
 app.listen(3000, () =>{
     console.log("Server started successfully")
